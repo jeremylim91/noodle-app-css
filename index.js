abd995f68042befc31216e0e30e860c66e35e81d
jeremylim91 <jeremylim_91@live.com>
2020-11-16 08:49:35 +0800

    complete base version

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { read } from './jsonFileStorage.js';
 const app = express();
 const PORT = 3004;
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 const getRecipeAtIndexX = (req, res) => {
   const chosenIndex = req.params.index;
@@ -16,7 +17,7 @@ const getRecipeAtIndexX = (req, res) => {
     }
     // if chosenIndex is larger than the length of data.recipes, it must be an invalid path;
     // additionally, if chosenIndex is not a number, it must be an invalid path;
-    else if ((chosenIndex > data.recipes.length) || (chosenIndex > 0 === false)) {
+    else if ((chosenIndex > data.recipes.length) || (chosenIndex >= 0 === false)) {
       // response.send(' Error 404- Page not found');
       res.status(404).send('Sorry, we cannot find that!');
       return;
@@ -51,13 +52,50 @@ const getRecipesWYieldNumX = (req, res) => {
 
 const displayMainPage = (req, res) => {
   read('data.json', (data) => {
+    // create a variable to allow easy access to the object's keys
     const objectToDisplay = data.recipes;
-    console.log(objectToDisplay[0]);
-    res.render('mainPage', { objectToDisplay });
+    const categoriesArray = [];
+    // store all categories in an array (ignore those that have no categories)
+
+    data.recipes.forEach((element) => {
+      if ('category' in element) {
+        categoriesArray.push(element.category);
+      }
+    });
+    // remove duplicate elements in the categories array
+    const uniqueCategoriesArray = [...new Set(categoriesArray)];
+    res.render('mainPage', { objectToDisplay, uniqueCategoriesArray });
+  });
+};
+
+const displayCategoryPage = (req, res) => {
+  const { categoryName } = req.params;
+  console.log('categoryName is:');
+  console.log(categoryName);
+  console.log('-end-');
+
+  read('data.json', (data) => {
+  // loop through each object in data.json
+  // create variable that easily acccesses the array wiithin the data.json
+    const recipeData = data.recipes;
+
+    // create a variable to store the objects with categories
+    const arrayOfRecipesToDisplay = [];
+    // loop within the array to identify which objects have a 'category';
+    recipeData.forEach((element) => {
+      if (('category' in element) && (element.category === categoryName)) {
+        // if the item contains 'category', add it to an object that you are going to display
+        arrayOfRecipesToDisplay.push(element);
+      }
+    });
+    const objectToDisplay = { arrayOfRecipesToDisplay, categoryName };
+    // console.log(arrayOfRecipesToDisplay);
+    res.render('categoryPage', { objectToDisplay });
   });
 };
 // listen on a port and execute a call-back when triggered
 app.get('/recipe/:index', getRecipeAtIndexX);
 app.get('/yield/:yieldNumber', getRecipesWYieldNumX);
 app.get('/', displayMainPage);
+app.get('/category/:categoryName', displayCategoryPage);
 app.listen(PORT);
